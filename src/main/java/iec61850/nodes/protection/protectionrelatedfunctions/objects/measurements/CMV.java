d2bd835ab5eccebf5285f223d61f5743f61701f2
Storozhuk Danil <danilstorojuk@yandex.ru>
2022-11-11 00:30:18 +0300

    Update CMV.java

--- src/main/java/iec61850/nodes/protection/protectionrelatedfunctions/objects/measurements/CMV.java
+++ src/main/java/iec61850/nodes/protection/protectionrelatedfunctions/objects/measurements/CMV.java
@@ -3,12 +3,6 @@ package iec61850.nodes.protection.protectionrelatedfunctions.objects.measurement
 import lombok.Getter;
 import lombok.Setter;
 
-/**
- * @author Александр Холодов
- * @created 03.2022
- * @project Programming2022
- * @description
- */
 @Getter @Setter
 public class CMV {
 
