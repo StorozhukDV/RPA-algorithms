7c30531a495dd5f52093a1bb0dfa2b9807c3293f
Storozhuk Danil <danilstorojuk@yandex.ru>
2022-11-11 00:30:51 +0300

    Update Direction.java

--- src/main/java/iec61850/nodes/protection/protectionrelatedfunctions/objects/protection/dir/Direction.java
+++ src/main/java/iec61850/nodes/protection/protectionrelatedfunctions/objects/protection/dir/Direction.java
@@ -1,11 +1,6 @@
 package iec61850.nodes.protection.protectionrelatedfunctions.objects.protection.dir;
 
-/**
- * @author Александр Холодов
- * @created 03.2022
- * @project Programming2022
- * @description
- */
+
 public enum Direction {
     UNKNOWN,
     FORWARD,
