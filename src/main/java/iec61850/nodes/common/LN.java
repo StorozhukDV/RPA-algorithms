02c1155ffe011ce9c404c35d75de06f08bcb0960
Storozhuk Danil <danilstorojuk@yandex.ru>
2022-11-11 00:27:46 +0300

    Update LN.java

--- src/main/java/iec61850/nodes/common/LN.java
+++ src/main/java/iec61850/nodes/common/LN.java
@@ -1,11 +1,5 @@
 package iec61850.nodes.common;
 
-/**
- * @author Александр Холодов
- * @created 03.2022
- * @project Programming2022
- * @description
- */
 public abstract class LN {
 
     /** Выполнить функцию РЗА */
